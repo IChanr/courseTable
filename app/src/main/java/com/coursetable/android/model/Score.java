b920be8e6dc72abc3ea2cf9ec2c801cefc944aa3
Ham <1078110811@qq.com>
2018-01-09 19:15:25 +0800

    第二个版本增加了 查询成绩功能  并完善了查询课表登陆界面的反馈

---
+++ app/src/main/java/com/coursetable/android/model/Score.java
@@ -0,0 +1,8 @@
+package com.coursetable.android.model;
+
+/**
+ * Created by H on 2018/1/8.
+ */
+
+public class Score {
+}
---
+++ app/src/main/java/com/coursetable/android/scoreAdapter.java
@@ -0,0 +1,10 @@
+package com.coursetable.android;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Created by H on 2018/1/9.
+ */
+
+public class scoreAdapter extends BaseAdapter {
+}
